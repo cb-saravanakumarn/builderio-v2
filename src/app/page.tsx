dcccb99e7f7048446c828b224c9a1ddc3e07fce6
cb-saravanakumarn <saravanakumar.n@chargebee.com>
2025-08-07 13:49:53 +0530

    comp style issues fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { BuilderComponent, builder } from "@builder.io/react";
+import { Button } from "builderio-sting-ds";
 
 // Replace with your actual Builder.io public key
 builder.init("7f58433da349460a86f2bfe8f11285ed");
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Builder.io Project</h1>
+    <div className="p-8 space-y-4">
       <BuilderComponent model="page" />
     </div>
   );
