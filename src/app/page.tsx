e3bb65a3927092fced3c78e76e2bbd7f9370feab
cb-saravanakumarn <saravanakumar.n@chargebee.com>
2025-08-07 14:54:55 +0530

    sting react added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
+import "./style.css";
 import { BuilderComponent, builder } from "@builder.io/react";
-import { Button } from "builderio-sting-ds";
-
+import { SButton } from "@chargebee/sting-react";
 // Replace with your actual Builder.io public key
 builder.init("7f58433da349460a86f2bfe8f11285ed");
 
 export default function Home() {
   return (
     <div className="p-8 space-y-4">
+      <SButton>Click me</SButton>
       <BuilderComponent model="page" />
     </div>
   );
